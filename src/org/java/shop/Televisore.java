3400a9c8027410255871271113360d6a066e2aed
Pietromuresu <muresupietro66@gmail.com>
2023-09-18 15:33:09 +0200

    Add Televisore subclass

---
+++ src/org/java/shop/Televisore.java
@@ -0,0 +1,33 @@
+package org.java.shop;
+
+public class Televisore extends Prodotto{
+	
+	public int dimensions;
+	public boolean smartTv;
+
+	public Televisore(String name, String brand, double price, float iva, int dimensions, boolean isSmart) {
+		
+		super(name, brand, price, iva);
+		setSmartTv(isSmart);
+		setDimensions(dimensions);
+	}
+	
+	public int getDimensions() {
+		return dimensions;
+	}
+	
+	public void setDimensions(int dimensions) {
+		if(dimensions > 0)
+			this.dimensions = dimensions;
+	}
+	
+	public void setSmartTv(boolean isSmart) {
+		
+		this.smartTv = isSmart;
+	}
+	
+	public boolean isSmart() {
+		
+		return smartTv;
+	}
+}
